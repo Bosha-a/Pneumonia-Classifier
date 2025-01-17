2ea903d3a0f5d9aca9216a517998ad0af72e50f6
Abdallah Beshary <abdallahuuuio221@gmail.com>
2025-01-18 01:08:15 +0200

    edit

--- app.py
+++ app.py
@@ -14,18 +14,18 @@ uploaded_file = st.file_uploader('Upload an X-ray image (PNG, JPG, JPEG)', type=
 
 if uploaded_file is not None:
 
-    image = Image.open(uploaded_file).convert('RGB')  
-    image = image.resize((150, 150))  
+    image = Image.open(uploaded_file).convert('L')  
+    image = image.resize((224, 224))  
     image_array = np.array(image) / 255.0 
     image_array = np.expand_dims(image_array, axis=0)  
 
-    if st.button('Predict'):
-        prediction = model.predict(image_array)[0][0]
-        predicted_class = 'Pneumonia' if prediction > 0.5 else 'Normal'
+#     if st.button('Predict'):
+#         prediction = model.predict(image_array)[0][0]
+#         predicted_class = 'Pneumonia' if prediction > 0.5 else 'Normal'
 
-        if predicted_class == 'Pneumonia':
-            st.error(f"Case: {predicted_class}")
-        else:
-            st.success(f"Case: {predicted_class}")
-else:
-    st.error("Please upload an image file (PNG, JPG, JPEG).")
+#         if predicted_class == 'Pneumonia':
+#             st.error(f"Case: {predicted_class}")
+#         else:
+#             st.success(f"Case: {predicted_class}")
+# else:
+#     st.error("Please upload an image file (PNG, JPG, JPEG).")
